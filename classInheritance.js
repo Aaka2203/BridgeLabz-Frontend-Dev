520a7779206c0027d1bb0074e43804698ae4fccf
Aaka2203 <aakarshsaxena0001@gmail.com>
2025-11-20 13:42:02 +0530

    [Aakarsh]:classInheritance.js

---
+++ classInheritance.js
@@ -0,0 +1,25 @@
+// classInheritance.js
+
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+  showName() {
+    console.log("Name:", this.name);
+  }
+}
+
+class Student extends Person {
+  constructor(name, branch) {
+    super(name);         // call parent constructor
+    this.branch = branch;
+  }
+  showBranch() {
+    console.log("Branch:", this.branch);
+  }
+}
+
+const s = new Student("Aakarsh", "CSE");
+
+s.showName();
+s.showBranch();
